006d3083d3b7055f29a9e7d5da120000cd5a0903
Jordan Hunt <jordan.hunt@g.austincc.edu>
2026-07-19 12:17:12 -0500

    Starting another project. Starting with  boilerplate Window stuff

---
+++ include/engine/window.h
@@ -0,0 +1,16 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+#include <SDL3/SDL.h>
+#include <stdbool.h>
+#include <stdlib.h>
+typedef struct Window {
+  SDL_Window *sdlWindow;
+  SDL_Renderer *sdlRenderer;
+  bool isQuit;
+} Window;
+
+Window *WindowCreate(const char *title, int width, int height);
+void WindowDestroy(Window *window);
+bool WindowPollEvents(Window *window);
+void WindowGetRenderer(Window *window);
+#endif

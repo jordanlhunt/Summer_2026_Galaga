b5751e7c628f4ad9272bb459a212adf0e06e819b
Jordan Hunt <jordan.hunt@g.austincc.edu>
2026-07-25 21:54:03 -0500

    Implemented timer.c

--- include/engine/timer.h
+++ include/engine/timer.h
@@ -1,8 +1,13 @@
 #ifndef TIMER_H
 #define TIMER_H
+#include <SDL3/SDL.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-typedef struct Timer Timer;
+typedef struct Timer {
+  uint64_t performanceFrequency;
+  uint64_t performanceCounter;
+} Timer;
 
 Timer *TimerCreate();
 void TimerDestroy(Timer *timer);
---
+++ src/engine/timer.c
@@ -0,0 +1,30 @@
+#include "../../include/engine/timer.h"
+
+Timer *TimerCreate() {
+  Timer *newTimer = malloc(sizeof(Timer));
+  if (newTimer == NULL) {
+    return NULL;
+  }
+  newTimer->performanceFrequency = SDL_GetPerformanceFrequency();
+  newTimer->performanceCounter = SDL_GetPerformanceCounter();
+  return newTimer;
+}
+void TimerDestroy(Timer *timer) { free(timer); }
+double TimerTick(Timer *timer) {
+  uint64_t currentPerformanceCounter = SDL_GetPerformanceCounter();
+  uint64_t deltaTime = currentPerformanceCounter - timer->performanceCounter;
+  return ((double)deltaTime / (double)timer->performanceFrequency);
+}
+void TimerCapFramesPerSecond(Timer *timer, double targetFps) {
+  double targetFramePerSecond = 1.0 / targetFps;
+  uint64_t now = SDL_GetPerformanceCounter();
+  double elapsedTime = (double)(now - timer->performanceCounter) /
+                       (double)timer->performanceFrequency;
+  if (elapsedTime < targetFramePerSecond) {
+    double sleepTime =
+        (targetFramePerSecond - elapsedTime) * 1000.0; // milliseconds
+    SDL_Delay((uint32_t)sleepTime);
+    timer->performanceCounter +=
+        (uint64_t)(sleepTime * timer->performanceFrequency / 1000.0);
+  }
+}

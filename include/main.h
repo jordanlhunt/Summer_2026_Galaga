793fb3ac620e4a23e2a91345a1ddd860ea211532
Jordan Hunt <jordan.hunt@g.austincc.edu>
2026-07-25 22:04:13 -0500

    Added Draw() and Update() functions to main for later use

---
+++ include/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define TARGET_UPDATES_PER_SECOND 60.0
+#define DELTA_TIME (1.0 / TARGET_UPDATES_PER_SECOND)
+#include "../include/engine/timer.h"
+#include "../include/engine/window.h"
+#include <stdio.h>
+#endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,21 @@
-#include "../include/engine/window.h"
+#include "../include/main.h"
+
+static double gameTime = 0.0;
+
+static void GameUpdate(double deltaTime) {
+  gameTime += deltaTime;
+  static double printTimer = 0.0;
+  printTimer += deltaTime;
+  if (printTimer >= 1.0) {
+    printf("Game time: %.2f seconds \n", gameTime);
+    printTimer = 0.0;
+  }
+}
+static void GameRender(SDL_Renderer *renderer) {
+  SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
+}
 
 int main(void) {
   Window *gameWindow =

6311f34fd7caca87c6e98e71bf1e31a230c5da48
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2026-07-27 11:41:33 -0500

    Added a common.h like i did in PAC-MAN for standard library functions and SDL3 stuff and/or constants

---
+++ include/common.h
@@ -0,0 +1,12 @@
+#ifndef COMMON_H
+#define COMMON_H
+// Standard C Library
+#include <assert.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+// SDL3
+#include <SDL3/SDL.h>

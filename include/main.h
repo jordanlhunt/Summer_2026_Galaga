d0b6dde4621e889d1796d8beb64c6b7abf05f605
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2026-07-28 13:40:24 -0500

    Added Input.h to main

--- include/main.h
+++ include/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #define TARGET_UPDATES_PER_SECOND 60.0
 #define DELTA_TIME (1.0 / TARGET_UPDATES_PER_SECOND)
+#include "../include/engine/input.h"
 #include "../include/engine/timer.h"
 #include "../include/engine/window.h"
 #include "common.h"

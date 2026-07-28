daad6986c7c54caeaa120587287eb78403350c7d
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2026-07-28 13:11:20 -0500

    Stared Input

--- include/engine/input.h
+++ include/engine/input.h
@@ -1,1 +1,15 @@
-// TODO
+#ifndef INPUT_H
+#define INPUT_H
+#include "../common.h"
+#define INPUT_NUM_KEYS SDL_SCANCODE_COUNT
+typedef struct Input {
+  bool currentKeys[INPUT_NUM_KEYS];
+  bool previousKeys[INPUT_NUM_KEYS];
+} Input;
+Input *InputCreate();
+void InputDestroy(Input *input);
+void InputUpdate(Input *input);
+bool InputIsKeyDown(Input *input, SDL_Scancode sdlScanCodeKey);
+bool InputIsKeyPressed(Input *input, SDL_Scancode sdlScanCodeKey);
+bool InputIsKeyReleased(Input *input, SDL_Scancode sdlScaneCodeKey);
+#endif
--- src/engine/input.c
+++ src/engine/input.c
@@ -1,1 +1,1 @@
-// TODO
+#include "engine/input.h"

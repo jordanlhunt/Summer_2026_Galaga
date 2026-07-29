01dd53266a20a29840b4d957bfb861de059d37ef
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2026-07-29 11:04:13 -0500

    Finished input.c

--- src/engine/input.c
+++ src/engine/input.c
@@ -1,1 +1,32 @@
 #include "engine/input.h"
+
+Input *InputCreate() {
+  Input *input = malloc(sizeof(Input));
+  if (input == NULL) {
+    return NULL;
+  }
+  memset(input->currentKeys, 0, sizeof(input->currentKeys));
+  memset(input->previousKeys, 0, sizeof(input->previousKeys));
+}
+void InputDestroy(Input *input) { free(input); }
+void InputUpdate(Input *input) {
+  // Move current frame's state into previous
+  memccpy(input->previousKeys, input->currentKeys, sizeof(input->previousKeys));
+  int numkeys;
+  const bool *currentKeyboardStateArray = SDL_GetKeyboardState(&numkeys);
+  int count;
+  if (numkeys < INPUT_NUM_KEYS) {
+    count = numkeys;
+  } else {
+    numkeys = INPUT_NUM_KEYS;
+  }
+  memcpy(input->currentKeys, currentKeyboardStateArray, (count * sizeof(bool)));
+}
+bool InputIsKeyDown(Input *input, SDL_Scancode sdlScanCode) {
+  return input->currentKeys[sdlScanCode];
+}
+
+bool InputIsKeyPressed(Input *input, SDL_Scancode sdlScanCode) {
+  return (input->currentKeys[sdlScanCode]) &&
+         !(input->previousKeys[sdlScanCode]);
+}

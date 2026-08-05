bfc229372667e82cc4e6b7aa50919920dcbf9ce6
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2026-08-05 11:44:30 -0500

    I can't seem to focus, I'll work more when I get home after work

--- src/engine/entity.c
+++ src/engine/entity.c
@@ -1,1 +1,46 @@
 #include "engine/entity.h"
+
+Entity entities[MAX_ENTITIES];
+int entityCount = 0;
+
+void EntityInitialize() {
+  memset(entities, 0, sizeof(entities));
+  entityCount = 0;
+}
+
+void EntitySpawn(int entityType, float x, float y, SDL_Texture *sdlTexture,
+                 int width, int height) {
+  for (int i = 0; i < MAX_ENTITIES; i++) {
+    if (entities[i].isActive == false) {
+      entities[i].isActive = true;
+      entities[i].entityType = entityType;
+      entities[i].positionX = x;
+      entities[i].positionY = y;
+      entities[i].sdlTexture = sdlTexture;
+      entities[i].width = width;
+      entities[i].height = height;
+      entities[i].hitpoints = 1;
+      entities[i].timer = 0.0f;
+      entityCount += 1;
+      return;
+    }
+  }
+  void EntityDestroy(Entity * entity) {
+    if (entity != NULL && entity->isActive) {
+      entity.isActive = false;
+      entityCount -= 1;
+    }
+  }
+  void EntityUpdateAll(float deltaTime) {
+    for (int i = 0; i < MAX_ENTITIES; i++) {
+      if (entities[i].isActive) {
+        entities[i].positionX += entities[i].velocityX * deltaTime;
+        entities[i].positionY += entities[i].velocityY * deltaTime;
+        if (entities[i].timer > 0.0f) {
+          entities[i].timer -= deltaTime;
+        }
+      }
+    }
+  }
+  void EntityRenderAll(float deltaTime) {}
+}
